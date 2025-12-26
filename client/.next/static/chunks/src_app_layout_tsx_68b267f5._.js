3779d28bccfb0a729b04ada167dcdfdac4fa280c
proavijit <avijitghosh2268@gmail.com>
2025-12-27 01:15:43 +0600

    feat: Initialize Next.js client application with UserButton component and dependency lockfile.

--- client/.next/static/chunks/src_app_layout_tsx_68b267f5._.js
+++ client/.next/static/chunks/src_app_layout_tsx_68b267f5._.js
@@ -2,8 +2,8 @@
     script: typeof document === "object" ? document.currentScript : undefined,
     chunks: [
   "static/chunks/[root-of-the-server]__50163470._.css",
-  "static/chunks/node_modules_c118b1b7._.js",
-  "static/chunks/src_88e230b1._.js"
+  "static/chunks/src_8de2d85e._.js",
+  "static/chunks/node_modules_39ae0a3a._.js"
 ],
     source: "dynamic"
 });
